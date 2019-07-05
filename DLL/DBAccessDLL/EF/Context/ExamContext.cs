716920fa2519ae88d79877fb584d0ace640b712a
LinQing <aa875191946@qq.com>
2019-07-05 18:37:34 +0800

    up

---
+++ DLL/DBAccessDLL/EF/Context/ExamContext.cs
@@ -0,0 +1,21 @@
+﻿using DBAccessDLL.EF.Context.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBAccessDLL.EF.Context
+{
+    /// <summary>
+    /// Test数据库
+    /// </summary>
+    public class ExamContext : BaseContext<ExamContext>
+    {
+        /// <summary>
+        /// TestContext
+        /// </summary>
+        /// <param name="options"></param>
+        public ExamContext(DbContextOptions<ExamContext> options, string _ConnString = "") 
+            : base(options, _ConnString)
+        {
+
+        }
+    }
+}
