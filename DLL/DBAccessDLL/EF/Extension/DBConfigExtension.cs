f68fe6629f97daa589403d181f4333ee2dbff37a
LinQing <aa875191946@qq.com>
2019-07-08 19:20:40 +0800

    up

---
+++ DLL/DBAccessDLL/EF/Extension/DBConfigExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DBAccessDLL.EF
+{
+    /// <summary>
+    /// 实体配置函数扩展
+    /// </summary>
+    static public class DBConfigExtension
+    {
+    }
+}
