21deb6982f3d8d9bfef7730e0e7d5be9ebf0fcbe
LinQing <aa875191946@qq.com>
2019-07-30 19:39:21 +0800

    up

---
+++ WebAPITempalte/FileUploadOperation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Swagger
+{
+    /// <summary>
+    /// Swagger 文件上传配置
+    /// </summary>
+    public class FileUploadOperation : IOperationFilter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="context"></param>
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (operation.OperationId.ToLower() == "apivaluesuploadpost")
+            {
+                operation.Parameters.Clear();
+                operation.Parameters.Add(new NonBodyParameter
+                {
+                    Name = "uploadedFile",
+                    In = "formData",
+                    Description = "Upload File",
+                    Required = true,
+                    Type = "file"
+                });
+                operation.Consumes.Add("multipart/form-data");
+            }
+        }
+    }
+}
