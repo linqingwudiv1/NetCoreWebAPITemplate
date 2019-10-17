aff362d1175691dade32d1300abe4f7198f0b466
LinQing <aa875191946@qq.com>
2019-10-17 18:01:49 +0800

    up

--- DLL/DBAccessDLL/EF/Extension/DBQueryExtension.cs
+++ DLL/DBAccessDLL/EF/Extension/DBQueryExtension.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Reflection;
 
 namespace DBAccessDLL.EF
 {
@@ -33,10 +35,31 @@ namespace DBAccessDLL.EF
         /// <param name="pageSize"></param>
         /// <param name="pageNum"></param>
         /// <returns></returns>
-        static public IQueryable<T> QueryPages<T>(this DbSet<T> query, int pageSize, int pageNum) where T: class
+        static public IQueryable<T> QueryPages<T>(this DbSet<T> query, int pageSize, int pageNum) where T : class
         {
             query.Skip(pageSize * (pageNum - 1)).Take(pageSize);
             return query;
         }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        static public string ToSql<T>(this IQueryable<T> query)
+        {
+            var enumerator = query.Provider.Execute<IEnumerable<T>>(query.Expression).GetEnumerator();
+            var enumeratorType = enumerator.GetType();
+            var selectFieldInfo = enumeratorType.GetField("_selectExpression", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException($"cannot find field _selectExpression on type {enumeratorType.Name}");
+            var sqlGeneratorFieldInfo = enumeratorType.GetField("_querySqlGeneratorFactory", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException($"cannot find field _querySqlGeneratorFactory on type {enumeratorType.Name}");
+            var selectExpression = selectFieldInfo.GetValue(enumerator) as SelectExpression ?? throw new InvalidOperationException($"could not get SelectExpression");
+            var factory = sqlGeneratorFieldInfo.GetValue(enumerator) as IQuerySqlGeneratorFactory ?? throw new InvalidOperationException($"could not get IQuerySqlGeneratorFactory");
+            var sqlGenerator = factory.Create();
+            var command = sqlGenerator.GetCommand(selectExpression);
+            var sql = command.CommandText;
+            return sql;
+        }
     }
+
 }
