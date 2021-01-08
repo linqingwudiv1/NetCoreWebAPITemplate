e15319aeaddfcce2998d7902b3a28c704695538d
LinQing <aa875191946@qq.com>
2021-01-08 19:38:52 +0800

    up

---
+++ DLL/SearchEngineDLL/LuceneHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SearchEngineDLL
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static public class LuceneHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="IndexPath"></param>
+        /// <param name="SourcePath"></param>
+        /// <param name="pattern"></param>
+        static void LoadUpdate(string IndexPath, string SourcePath, string [] SourceDirePattern) 
+        {
+        }
+    }
+}
